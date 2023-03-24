519d62775d9ecbefecfbf72f165816240c0fdcac
Michiel van der Meer <contact@michielvandermeer.com>
2023-03-24 21:25:01 +0100

    initial commit

---
+++ mvdmio.Tailwind.NET/lib/contentFiles/any/any/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
---
+++ mvdmio.Tailwind.NET/scripts/download-tailwindcli.ps1
@@ -0,0 +1,34 @@
+param(
+  [string] $version = ""
+)
+
+$response = gh release list -R tailwindlabs/tailwindcss
+$releases = $response | ForEach-Object {
+    $columns = $_.Trim() -split "\s+", 4
+    [PSCustomObject]@{
+        Title     = $columns[0]
+        Type      = $columns[1]
+        TagName   = $columns[2]
+        Published = $columns[3]
+    }
+}
+
+if($version -eq "") {
+  # Download latest version
+  $version = $releases[0].Title
+}
+
+Write-Host "Downloading: $version"
+
+$selectedRelease = $releases | Where-Object { $_.Title -eq $version }
+
+if($selectedRelease -eq $null){
+  Write-Host "Could not find release '$version'"
+  Write-Host "Available releases:"
+  Write-Host $releases | Format-Table -AutoSize
+  return
+}
+
+$basePath = Split-Path $MyInvocation.MyCommand.Path -Parent
+gh release download -p "*" $version -D "$basePath/../../tools" -R tailwindlabs/tailwindcss --clobber
+$discard = New-Item "$basePath/../../tools/$version" -Force

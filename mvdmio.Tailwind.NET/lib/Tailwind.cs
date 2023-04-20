eb7c3e05b5f80235e7d765b08de71d363f97de09
Michiel van der Meer <contact@michielvandermeer.com>
2023-04-20 21:30:51 +0200

    Trying to start a separate tailwind-watch cmd window from the build targets

---
+++ mvdmio.Tailwind.NET/lib/Tailwind.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace mvdmio.Tailwind.NET.lib;
+
+/// <summary>
+/// Helper class for Tailwind methods
+/// </summary>
+public static class Tailwind
+{
+   /// <summary>
+   /// Runs the BuildTailwind target and prints the output to the console.
+   /// </summary>
+   public static void RunTailwindBuild()
+   {
+      RunOnCommandLine("dotnet msbuild -target:BuildTailwind");
+   }
+
+   /// <summary>
+   /// Runs the WatchTailwind target and prints the output to the console.
+   /// </summary>
+   public static void RunTailwindWatch()
+   {
+      RunOnCommandLine("dotnet msbuild -target:WatchTailwind");
+   }
+
+   private static void RunOnCommandLine(string command)
+   {
+      var process = new Process();
+      process.StartInfo.FileName = "cmd.exe";
+      process.StartInfo.RedirectStandardOutput = true;
+      process.StartInfo.Arguments = $"/C {command} ";
+      process.StartInfo.CreateNoWindow = false;
+      process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+      process.StartInfo.UseShellExecute = false;
+      process.Start();
+      
+      var output = process.StandardOutput.ReadToEnd();
+      Console.WriteLine(output);
+   }
+}

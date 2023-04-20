42fa6e6e6e5a0a6c32006e011c89e25e1267a035
Michiel van der Meer <contact@michielvandermeer.com>
2023-04-20 20:38:32 +0200

    Added TailwindWatch target

---
+++ mvdmio.Tailwind.NET/lib/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace mvdmio.Tailwind.NET.lib.Extensions;
+
+/// <summary>
+/// Helper class for Tailwind methods
+/// </summary>
+public static class Tailwind
+{
+   public static void RunTailwindBuild()
+   {
+      var cmd = CommandLine;
+      cmd.StandardInput.WriteLine("msbuild -target:BuildTailwind");
+   }
+
+   public static void RunTailwindWatch()
+   {
+      var cmd = CommandLine;
+      cmd.StandardInput.WriteLine("msbuild -target:WatchTailwind");
+   }
+
+   private static Process CommandLine
+   {
+      get
+      {
+         var process = new Process();
+         process.StartInfo.FileName = "cmd.exe";
+         process.StartInfo.RedirectStandardInput = true;
+         process.StartInfo.RedirectStandardOutput = true;
+         process.StartInfo.UseShellExecute = false;
+         process.Start();
+
+         return process;
+      }
+   }
+}
